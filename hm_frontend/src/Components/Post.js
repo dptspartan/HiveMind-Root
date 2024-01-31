675e3369ab603a1bf97b89100fa3216c44bb8c88
Shoaib Rashid <shoaib.lahore90@gmail.com>
2024-01-31 23:42:04 +0500

    post are showing in frontend

--- hm_frontend/src/Components/Post.js
+++ hm_frontend/src/Components/Post.js
@@ -5,7 +5,7 @@ export default function Post({ foundpost }) {
   return (
     <div>
         <p>{user}</p>
-        <img src={`https://github.com/dptspartan/HiveMind-Root/blob/371156c82cb76c7c663cf1957946a4189cb3c978/HM_backend/media/${image}`} alt="Image Description" />
+        <img src={`http://127.0.0.1:8000/media/${image}`} alt="Image Description" />
         <p>{caption}</p>
         <p>{no_of_likes}</p>
     </div>
